18d392004cb77b8eca97249ec85ae8ef6aa535d1
Eduardo Diaz <lalo.diaz1896@gmail.com>
2021-08-12 20:15:56 -0500

    Test library

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './hero.module.css'
 import icon from "../../assets/airplane2.svg"
+import { CardExpandable, InputCheckbox, InputCreditCard } from 'schola-components'
 
 // Receiving prop from App. It will display the airline name state
 function Hero({ airline }) {
@@ -12,6 +13,9 @@ function Hero({ airline }) {
       </h1>
       <h4 className={styles.instruction}>Por favor, completa el siguiente formulario:</h4>
       <span className={styles.warning}>Todos los campos son obligatorios*</span>
+      <CardExpandable>Hola</CardExpandable>
+      <InputCheckbox>hehe</InputCheckbox>
+      <InputCreditCard />
     </div>
   )
 }
